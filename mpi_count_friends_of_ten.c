fe35b00eb085afa07f0ad2428cf45001514723d2
bernharl <b.n.lotsberg@fys.uio.no>
2020-05-06 14:19:38 +0200

    Basic mpi framework works

--- mpi_count_friends_of_ten.c
+++ mpi_count_friends_of_ten.c
@@ -1,3 +1,29 @@
 int MPI_count_friends_of_ten(int M, int N, int** v) {
-    
+    int rank;
+    int size;
+    int N_split;
+    int num_triple_friends = 0;
+    int N_rest = 0;
+    MPI_Comm_size (MPI_COMM_WORLD, &size);
+    MPI_Comm_rank (MPI_COMM_WORLD, &rank);
+    printf("%i %i\n", rank, size);
+    if (rank==0) {
+        N_split = N / size;
+        int** v_0 = allocate_2d(M, N_split);
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N_split; j++) {
+                v_0[i][j] = v[i][j];
+            }
+        }
+        num_triple_friends = count_friends_of_ten(M, N_split, v_0);
+        
+        
+        return num_triple_friends;
+        
+        
+    }
+    else {
+        0;
+    }
+    return 1;
 }
--- mpi_main.c
+++ mpi_main.c
@@ -1,26 +1,35 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <mpi.h>
 
-#include "mpi_count_friends_of_ten.c"
 #include "utils.c"
+#include "count_friends_of_ten.c"
+#include "mpi_count_friends_of_ten.c"
+
 
 int main(int argc, char **argv) {
-    /*int M=0, N=0, rank, num_triple_friends;
+    int M=0, N=0, rank, num_triple_friends;
     int **v=NULL;
     MPI_Init (&argc, &argv);
     MPI_Comm_rank (MPI_COMM_WORLD, &rank);
     if (rank==0) {
+        M = 100;
+        N = 100;
+        v = random_array(M, N);
     // decide the values for M and N
     // allocate 2D array v and assign it with suitable values
     }
     num_triple_friends = MPI_count_friends_of_ten (M, N, v);
     printf("MPI rank <%d>: number of triple friends=%d\n",rank, num_triple_friends);
     if (rank==0) {
+        printf("%i ??\n", v[0][0]);
+        free(v[0]);
+        free(v);
     // deallocation of 2D array 
     }
-    MPI_Finalize();*/
-    int M = 10;
+    MPI_Finalize();
+    /*int M = 10;
     int N = 10;
     int** A = random_array(M, N);
     for (int i = 0; i < M; i++) {
@@ -28,6 +37,6 @@ int main(int argc, char **argv) {
             printf("%i ", A[i][j]);
         }
         printf("\n");
-    }
+    }*/
     return 0;
 }
